bd32ce2a79880e9535b237d82ae410d755e71398
Alexander Mitchell <amitchellofficial@gmail.com>
2018-12-20 15:37:54 -0500

    adds the ability to replay the sound when you want

--- app.js
+++ app.js
@@ -30,15 +30,20 @@ function addSoundsToPage(sounds) {
 		soundTitle.textContent = sound.title;
 		soundDiv.appendChild(soundTitle);
 
+		// This creates my button and applies it to each sound
 		const playButton = document.createElement('button');
 		playButton.textContent = '🌊';
 		soundDiv.appendChild(playButton);
 
+		// This gets the actual sound and applies it to the cell with the same name
 		const player = document.createElement('audio');
 		player.setAttribute('src', `sounds/${sound.src}`);
 		soundDiv.appendChild(player);
 
+		// This will play the sound when the button is clicked
 		playButton.addEventListener('click', () => {
+			// This will allow me to start the sound over if clicked more than once
+			player.currentTime = 0;
 			player.play();
 		});
 
